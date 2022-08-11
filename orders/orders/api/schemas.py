e4a6cbee77000a8e5d8ab50d3828b4a920a958ea
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-08-11 04:38:14 +0300

    create schema for order

---
+++ orders/orders/api/schemas.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from typing import List
+from uuid import UUID
+
+from pydantic import BaseModel,Field
+
+class SizeEnum(str,Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class StatusEnum(str,Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+class OrderItemSchema(BaseModel):
+    product = str
+    size = SizeEnum
+    quantity = int = Field(default=1,example = 1)
+
+class CreateOrderSchema(BaseModel):
+    order:List[OrderItemSchema]
+
+class GetOrderSchema(CreateOrderSchema):
+    id : UUID
+    created : int = Field(description='Date in the form of UNIX timestamp')
+
